020dfbee84a71dc020c72fce82af1844c824c477
MichelMiertschink <chereu.m@gmail.com>
2024-04-12 23:30:23 -0300

    States Delete / Details - incomplete

--- SerranaLogCargas/Controllers/StatesController.cs
+++ SerranaLogCargas/Controllers/StatesController.cs
@@ -32,5 +32,44 @@ namespace SerranaLogCargas.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        public IActionResult Delete (int? id) 
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _stateService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete (int? id)
+        {
+
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _stateService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
     }
 }
--- SerranaLogCargas/Services/StateService.cs
+++ SerranaLogCargas/Services/StateService.cs
@@ -17,9 +17,23 @@ namespace SerranaLogCargas.Services
             return _context.States.ToList();
         }
 
+        public State FindById(int id) 
+        {
+            return _context.States.FirstOrDefault(obj => obj.Id == id);
+        }
+
         public void Insert(State obj)
         {
             _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public void Update(State obj) { }
+
+        public void Remove(int id)
+        {
+            var obj = _context.States.Find(id);
+            _context.States.Remove(obj);
             _context.SaveChanges();
         }
     }

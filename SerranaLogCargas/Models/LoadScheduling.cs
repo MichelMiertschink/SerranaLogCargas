9ec5841a3f0d5472dba67e331f3f3d37aa63d87b
MichelMiertschink <chereu.m@gmail.com>
2024-04-22 22:04:22 -0300

    Criação do model de carregamento

---
+++ SerranaLogCargas/Models/LoadScheduling.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SerranaLogCargas.Models
+{
+    public class LoadScheduling
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "BOL")]
+        public bool Bol {  get; set; }
+
+        [Display(Name = "Cliente")]
+        public Customer Customer { get; set; }
+        public int CustomerId {  get; set; }
+
+        [Display(Name = "Origem")]
+        public City CityOrigin {  get; set; }
+        public int OriginId { get; set; }
+        
+        [Display(Name = "Destino")]
+        public City CityDestiny { get; set; }
+        public int DestinyId { get; set; }
+
+        [Display(Name = "Dt. Desc.")]
+        public DateTime UnloadDate { get; set; }
+
+        // Vários campos com possíveis Enums
+        // G
+        // C
+        // CK
+        // M
+        [Display(Name = "Peso")]
+        public float Weigth { get; set; }
+
+        [Display(Name = "Tranpor")]
+        public float VlTranspor {  get; set; }
+
+        [Display(Name = "Vl Contrato")]
+        public float VlContract {  get; set; }
+
+        [Display(Name = "Vl Adiant.")]
+        public float Vladvance {  get; set; }
+
+        [Display(Name = "R$")]
+        public bool Pay {  get; set; }
+
+        [Display(Name = "Contrato")]
+        public int ContractId { get; set; }
+
+        [Display(Name = "CT-e")]
+        public bool Cte {  get; set; }
+
+    }
+}

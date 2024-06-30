a82849f5e9593f76922f98978281d0c73c69d3d1
MichelMiertschink <chereu.m@gmail.com>
2024-06-30 19:35:55 -0300

    Models - Index e Privacy

---
+++ SerranaLogCargas/Views/Home/Index.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
+
+namespace LogCargas.Views.Home;
+[AllowAnonymous]
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+    public void OnGet() { }
+
+}
